b61ecfe13090777e882cf4f63ec1f1e702464b6a
Tibowl <Tibowl@users.noreply.github.com>
2022-07-10 17:02:43 +0200

    Add channelorder restore

--- src/commands/channels/channelorder.ts
+++ src/commands/channels/channelorder.ts
@@ -1,4 +1,7 @@
-import { CommandInteraction, Message, MessageAttachment, NonThreadGuildBasedChannel, ThreadChannel, User } from "discord.js"
+import { ChannelPosition, CommandInteraction, Message, MessageAttachment, NonThreadGuildBasedChannel, ThreadChannel, User } from "discord.js"
+import { getLogger } from "log4js"
+import fetch from "node-fetch"
+import client from "../../main"
 import Command from "../../utils/Command"
 import { ROLE } from "../../utils/TicketTypes"
 import { CommandSource, SendMessage } from "../../utils/Types"
@@ -6,6 +9,7 @@ import { displayTimestamp, sendMessage } from "../../utils/Utils"
 
 export const channelUpdates: Record<string, {time: number, msg: string}[] | undefined> = {}
 
+const logger = getLogger("channelOrder")
 export default class ChannelOrder extends Command {
     constructor(name: string) {
         super({
@@ -22,13 +26,23 @@ export default class ChannelOrder extends Command {
                 name: "dump",
                 description: "Dump current channel positions",
                 type: "SUB_COMMAND"
+            }, {
+                name: "restore",
+                description: "Restore channel positions from data file",
+                type: "SUB_COMMAND",
+                options: [{
+                    name: "data",
+                    description: "Dump to restore from",
+                    type: "ATTACHMENT",
+                    required: true
+                }]
             }]
         })
     }
 
     async runInteraction(source: CommandInteraction): Promise<SendMessage | undefined> {
         await source.deferReply({ ephemeral: true })
-        return this.run(source, source.user, source.options.getSubcommand(true))
+        return this.run(source, source.user, source.options.getSubcommand(true), source.options.getAttachment("data", false))
     }
 
     async runMessage(source: Message, args: string[]): Promise<SendMessage | undefined> {
@@ -38,7 +52,7 @@ export default class ChannelOrder extends Command {
         return this.run(source, source.author, args[0].toLowerCase())
     }
 
-    async run(source: CommandSource, user: User, command: string): Promise<SendMessage | undefined> {
+    async run(source: CommandSource, user: User, command: string, attachment?: MessageAttachment | null): Promise<SendMessage | undefined> {
         if (!source.guild) return await sendMessage(source, "Couldn't get guild data", undefined, true)
 
         const member = await source.guild.members.fetch(user.id)
@@ -53,11 +67,14 @@ export default class ChannelOrder extends Command {
 
         const sorted = nonThread.sort((a, b) => a.position - b.position || a.id.localeCompare(b.id))
 
+        const me = await source.guild.members.fetch(client.user?.id ?? "0")
+
         const channelInfo = (x: NonThreadGuildBasedChannel) => ({
             id: x.id,
             position: x.rawPosition,
             name: x.name,
-            type: x.type
+            type: x.type,
+            canMove: x.permissionsFor(me).has("MANAGE_CHANNELS")
         })
 
 
@@ -123,9 +140,104 @@ ${channelUpdates[source.guild.id]?.map(r => `${displayTimestamp(new Date(r.time)
 ${response.join("\n") || "*No inconsistencies found!*"}`.substring(0, 1900), undefined, true)
         } else if (command == "dump") {
             await member.send({
-                files: [new MessageAttachment(Buffer.from(JSON.stringify(data, undefined, 4)), "channels.json")]
+                files: [new MessageAttachment(Buffer.from(JSON.stringify(data, undefined, 4)), `channels-${source.guild.id}-${new Date().toISOString().replace(/:|T/g, "-").replace("Z", "")}.json`)]
             })
             return await sendMessage(source, "Send in DMs!", undefined, true)
+        } else if (command == "restore" && attachment) {
+            if (!attachment.contentType?.startsWith("application/json"))
+                return await sendMessage(source, `Invalid content type ${attachment.contentType}, expected application/json`, undefined, true)
+
+            const dump = await (await fetch(attachment.url)).json() as typeof data
+
+            const movement: ChannelPosition[] = []
+            const cantMove: ChannelPosition[] = []
+
+            let lastText = 0, lastVoice = 0
+
+            for (const category of data) {
+                const dumpCategory = dump.find(x => x.id == category.id)
+
+                // eslint-disable-next-line no-inner-declarations
+                function justAdd(channels: typeof category.text, position: number) {
+                    for (const channel of channels) {
+                        if (channel.position != position)
+                            if (channel.canMove)
+                                movement.push({ channel: channel.id, position })
+                            else
+                                cantMove.push({ channel: channel.id, position })
+                        position++
+                    }
+                    return position
+                }
+
+                if (dumpCategory == null) {
+                    lastText = justAdd(category.text, lastText)
+                    // lastVoice = justAdd(category.vc, lastVoice)
+                    continue
+                }
+
+                // eslint-disable-next-line no-inner-declarations
+                function merge(categoryChannels: typeof category.text, dumpChannels: typeof category.text, position: number) {
+                    const newOrder = categoryChannels
+                        .filter(x => dumpChannels.some(n => n.id == x.id))
+                        .sort((a, b) => dumpChannels.findIndex(c => c.id == a.id) - dumpChannels.findIndex(c => c.id == b.id))
+
+                    const unique = categoryChannels.filter(x => !dumpChannels.some(n => n.id == x.id))
+
+                    uniqueLoop: for (const channel of unique) {
+                        const originalIndex = categoryChannels.indexOf(channel)
+                        if (originalIndex == 0) {
+                            newOrder.unshift(channel)
+                            continue uniqueLoop
+                        }
+                        const prevChannel = categoryChannels[originalIndex - 1]
+
+                        for (let i = 0; i < newOrder.length; i++) {
+                            if (newOrder[i] == prevChannel) {
+                                newOrder.splice(i + 1, 0, channel)
+                                continue uniqueLoop
+                            }
+                        }
+                    }
+
+                    /*
+                    console.log("Source: " + categoryChannels.map(i => i.name).join("/"))
+                    console.log("Dump: " + dumpChannels.map(i => i.name).join("/"))
+                    console.log("Unique: " + unique.map(i => i.name).join("/"))
+                    console.log("New order: " + newOrder.map(i => i.name).join("/"))
+                    console.log("==============")
+                    */
+
+                    for (const channel of newOrder) {
+                        if (channel.position != position)
+                            if (channel.canMove)
+                                movement.push({ channel: channel.id, position })
+                            else
+                                cantMove.push({ channel: channel.id, position })
+                        position++
+                    }
+
+                    return position
+                }
+
+
+                lastText = merge(category.text, dumpCategory.text, lastText)
+                lastVoice = merge(category.vc, dumpCategory.vc, lastVoice)
+            }
+
+            logger.info(`Moving ${movement.length} channels on request by ${member.user.tag}`)
+
+            try {
+                await source.guild.channels.setPositions(movement)
+
+                return await sendMessage(source, `**Unable to move (due to permissions)**
+${cantMove.map(m => `<#${m.channel}> to #${m.position}`).join("\n")}
+
+**Moved**
+${movement.map(m => `<#${m.channel}> to #${m.position}`).join("\n")}`.substring(0, 1900), undefined, true)
+            } catch (error) {
+                return await sendMessage(source, "Could not move channels!")
+            }
         }
 
         return await sendMessage(source, "Unknown subcommand", undefined, true)
