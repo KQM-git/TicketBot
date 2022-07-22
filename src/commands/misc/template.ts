086263c0b717ddd8c16aa1abaa5a4f85f2bcd9ce
Tibowl <Tibowl@users.noreply.github.com>
2022-07-22 23:48:34 +0200

    Add Edit template button

--- src/commands/misc/template.ts
+++ src/commands/misc/template.ts
@@ -1,4 +1,5 @@
-import { ButtonInteraction, CommandInteraction, Message, MessageActionRow, MessageEmbed, Modal, ModalSubmitInteraction, TextInputComponent, TextInputStyleResolvable } from "discord.js"
+import { ButtonInteraction, CommandInteraction, Message, MessageActionRow, MessageContextMenuInteraction, MessageEmbed, Modal, ModalSubmitInteraction, TextInputComponent, TextInputStyleResolvable } from "discord.js"
+import client from "../../main"
 import Command from "../../utils/Command"
 import { ROLE } from "../../utils/TicketTypes"
 import { InteractionSource, SendMessage } from "../../utils/Types"
@@ -9,6 +10,7 @@ const templates: Record<string, {
     embedTitle: string
     fields: {
         id: string
+        inline?: true
         modalTitle: string
         embedTitle: string
         modalPlaceholder?: string
@@ -72,6 +74,22 @@ const templates: Record<string, {
 - Rotation video (required if team calc), links can be formatted like [this](https://youtu.be/dQw4w9WgXcQ)
 - Additional details (if necessary)`,
             type: "PARAGRAPH"
+        }, {
+            id: "status",
+            inline: true,
+            modalTitle: "Status",
+            embedTitle: "Status",
+            modalPlaceholder: "Open / Under verification / etc.",
+            modalDefault: "Open",
+            type: "SHORT"
+        }, {
+            id: "participants",
+            inline: true,
+            modalTitle: "Participants",
+            embedTitle: "Participants",
+            modalPlaceholder: "Participants",
+            modalDefault: "This can be you!",
+            type: "PARAGRAPH"
         }]
     }
 }
@@ -92,7 +110,10 @@ export default class Template extends Command {
                     name, value
                 })),
                 required: true
-            }]
+            }],
+            onMessage: [
+                "Edit template"
+            ]
         })
     }
 
@@ -102,6 +123,52 @@ export default class Template extends Command {
 
     async runButton(source: ButtonInteraction): Promise<void> {
         await this.createModal(source, source.customId.split("-")[1])
+    }
+
+    async runMessageContext(source: MessageContextMenuInteraction, _command: string): Promise<void> {
+        const msg = source.targetMessage
+        if (msg.author.id !== client.user?.id || msg.embeds.length != 1) return
+        const embed = msg.embeds[0]
+
+        const member = await source.guild?.members.fetch(source.user.id)
+        if (!member) {
+            await sendMessage(source, "Couldn't fetch your roles", undefined, true)
+            return
+        }
+
+        if (member.roles.cache.hasAny(...ROLE.BLACKLIST)) {
+            await sendMessage(source, "You are blacklisted from using templates", undefined, true)
+            return
+        }
+
+        if (msg.interaction?.user.id != source.user.id && !member.roles.cache.hasAny(...ROLE.TC_STAFF)){
+            await sendMessage(source, "You don't have permissions to edit this template", undefined, true)
+            return
+        }
+
+        const templateName = Object.entries(templates).find(([_name, template]) => template.embedTitle == embed.title)?.[0]
+        if (!templateName) {
+            await sendMessage(source, "Could not find the relevant template!", undefined, true)
+            return
+        }
+
+        const template = templates[templateName]
+        const modal = new Modal()
+            .setTitle(`Editing a ${template.name}`)
+            .setCustomId(`template-${templateName}-${msg.id}`)
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            .setComponents(...template.fields.map(p => new MessageActionRow().addComponents(new TextInputComponent()
+                .setCustomId(p.id)
+                .setLabel(p.modalTitle)
+                .setStyle(p.type)
+                .setPlaceholder(p.modalPlaceholder ?? "")
+                .setValue(embed.fields?.find(f => f.name == p.embedTitle)?.value ?? p.modalDefault ?? "")
+                .setRequired(true)
+                .setMaxLength(1000)
+            )))
+
+        await source.showModal(modal)
     }
 
     async createModal(source: InteractionSource, templateName: string) {
@@ -148,14 +215,15 @@ export default class Template extends Command {
             return
         }
 
-        const templateName = source.customId.split("-")[1]
+        const split = source.customId.split("-")
+        const templateName = split[1]
         const template = templates[templateName]
         if (!template) {
             await sendMessage(source, "Could not find a template by that name!", undefined, true)
             return
         }
 
-        await sendMessage(source, new MessageEmbed()
+        const embed = new MessageEmbed()
             .setAuthor({
                 name: source.user.tag,
                 iconURL: source.user.avatarURL() ?? undefined
@@ -164,9 +232,27 @@ export default class Template extends Command {
             .setColor(Colors.GREEN)
             .addFields(template.fields.map(p => ({
                 name: p.embedTitle,
-                value: source.fields.getTextInputValue(p.id)
+                value: source.fields.getTextInputValue(p.id),
+                inline: p.inline
             })))
-        )
+
+        const msgId = split[2]
+        if (msgId) {
+            const msg = await source.channel?.messages.fetch(msgId)
+            if (!msg) {
+                await sendMessage(source, "Could not find the message this belonged to!", undefined, true)
+                return
+            }
+
+            if (msg.embeds[0].author)
+                embed.setAuthor(msg.embeds[0].author)
+
+            await msg.edit({ embeds: [embed] })
+            await sendMessage(source, "Edited!", undefined, true)
+            return
+        }
+
+        await sendMessage(source, embed)
     }
 
     async runMessage(source: Message): Promise<SendMessage | undefined> {
